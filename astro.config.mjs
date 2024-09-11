ca6af4b348b6ef6b0cbeff871658643d0d60f231
Santos Alarcón <santosalarcon86@gmail.com>
2024-09-11 12:14:31 +0200

    config: fix asset loading in GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://santosalarcon.github.io/gallery-columns-demo/",
+    site: "https://santosalarcon.github.io",
+    base: "/gallery-columns-demo/",
 });
